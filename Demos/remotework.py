f18f35668dbb63f57415697f4d43046c6d0f0c94
Tina Zhao <tinazhao@berkeley.edu>
2018-06-17 20:48:38 -0700

    Edited remotework.py

--- Demos/remotework.py
+++ Demos/remotework.py
@@ -4,12 +4,19 @@ import csv
 import datetime
 import new_db
 
+'''
+Basic Web Scraper (Built with Requests and BeautifulSoup)
+----------------------------------------------------------
+Before You Start:
+1. Check site's robot.txt.
+2. Browse source code of the site
+'''
 
-#Basic Web Scraper (uses Requests and BeautifulSoup)
-
-#Requests the webpage and checks for errors
-def get_webpage(url):
-	res = requests.get(url)
+'''
+Requests the webpage and checks for errors
+'''
+def get_webpage():
+	res = requests.get('https://weworkremotely.com/categories/remote-programming-jobs')
 	now = datetime.datetime.now()
 	try:
 		res.raise_for_status()
@@ -18,7 +25,7 @@ def get_webpage(url):
 	print('Updated as of '  + now.strftime('%m/%d/%y %I:%M:%f'))
 	return res
 
-#Use Beautiful Soup to select data
+#Use Beautiful Soup to return data
 def get_data(res, *classId):	
 	soup = bs4.BeautifulSoup(res.text, "lxml")
 	columns = []
@@ -29,7 +36,7 @@ def get_data(res, *classId):
 
 
 #run this
-webpage = get_webpage('https://weworkremotely.com/categories/remote-programming-jobs')
+webpage = get_webpage()
 data = get_data(webpage, 'company')
 #new_db.create_table('Remote_work', 'Company', 'TEXT')
 #new_db.populate_table('Remote_work', 'Company', data)
