5f2d94176d33af6ff046be03c347d3bab035589a
Tina Zhao <tinazhao@berkeley.edu>
2018-07-08 07:06:52 -0700

    Finished remotework and added to script

--- Demos/remotework.py
+++ Demos/remotework.py
@@ -11,27 +11,28 @@ Before You Start:
 
 #Requests the webpage and checks for errors
 def get_webpage(url):
-	res = requests.get(url)
-	try:
-		res.raise_for_status()
-	except Exception as e:
-		print(e)
-	return res
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+    except Exception as e:
+        print(e)
+    return res
 
 #Use Beautiful Soup to return data based off classId
-def get_data(res, *classId):	
-	soup = bs4.BeautifulSoup(res.text, "lxml")
-	columns = []
-	for c in classId: 
-		col = soup.select('.' + c)
-		columns.append(col)
-	columns.append(find_links(res))
-	return columns
+def get_data(res, *classId):    
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    columns = []
+    for c in classId: 
+        col = soup.select('.' + c)
+        columns.append(col)
+    columns.append(find_links(res))
+    return columns
 
 def find_links(res):
     soup = bs4.BeautifulSoup(res.text, "lxml")
     link_lst = []
     for a in soup.find_all('a', href=True):
-        if "/remote-jobs/" in a['href']:
-            link_lst.append(a['href'])
+        link = a['href']
+        if "/remote-jobs/" in link and "https://weworkremotely.com" not in link:
+            link_lst.append(link)
     return link_lst
--- Demos/static_script.py
+++ Demos/static_script.py
@@ -1,25 +1,9 @@
 import new_db
 from remotework import get_webpage, get_data, find_links
 
+new_db.drop_table("Remote_work")
 webpage = get_webpage("https://weworkremotely.com/categories/remote-programming-jobs")
 data = get_data(webpage, 'company', 'title')
-print(len(data))
-print("")
-print(data[0])
-print("")
-print(data[1])
-print("")
-print(data[2])
-#new_db.create_table('Remote_work', ['Company', 'Position', 'Link'], ['TEXT', 'TEXT', 'TEXT'])
+new_db.create_table('Remote_work', ['Company', 'Position', 'Link'], ['TEXT', 'TEXT', 'TEXT'])
 new_db.populate_table('Remote_work', data)
 new_db.print_table('Remote_work')
-
-
-
-#data = get_data(webpage, 'company', 'title')
-
-
-
-#new_db.create_table('Remote_work', ['Company', 'Position'], ['TEXT', 'TEXT'])
-#new_db.populate_table('Remote_work', data)
-#new_db.list_tables()
